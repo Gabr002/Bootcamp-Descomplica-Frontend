f90c308fde6a871c5447935abde6876670a80d15
owie <98811096+Gabr002@users.noreply.github.com>
2023-07-15 10:01:03 -0300

    Melhorias no projeto.
    Adicionei mais algumas funcionalidades como componentes do Bootstrap:
    - Button
    - Badge
    - Row
    - Col
    - Container

--- project/src/pages/Home/HomePage.js
+++ project/src/pages/Home/HomePage.js
@@ -1,9 +1,18 @@
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 import { Card } from "../../components/Card/Card"
 
 export const HomePage = () => {
     return(
-        <div className='HomePage'>
-            <Card />     
-        </div>
+        <Container>
+            <Row>
+                <Col xs={12} md={3}><Card /></Col>
+                <Col xs={12} md={3}><Card /></Col>
+                <Col xs={12} md={3}><Card /></Col>
+                <Col xs={12} md={3}><Card /></Col>
+                
+            </Row>
+        </Container>
     )
 }
