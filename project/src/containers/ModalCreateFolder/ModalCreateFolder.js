07e6374dcfd486d91dafdb29475b4742533abdef
owie <98811096+Gabr002@users.noreply.github.com>
2023-07-18 10:54:01 -0300

    Melhoria refeita no ModalCreateFolder.

--- project/src/containers/ModalCreateFolder/ModalCreateFolder.js
+++ project/src/containers/ModalCreateFolder/ModalCreateFolder.js
@@ -1,27 +1,40 @@
+import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import { Modal } from "../../components/Modal/Modal";
 
-export const ModalCreateFolder = (open) => {
+export const ModalCreateFolder = ({ open }) => {
+    const [ folderName, setFolderName ] = useState({});
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('')
     }
+
+    const handleChange = (e) => {
+        setFolderName(e.target.value);
+        console.log('Fez o submit', setFolderName);
+    }
+
     return(
         <Modal
             open={open}
             title="Criar pasta"
+            type='submit'
             controls={[
                 {
                     label: 'Criar e salvar',
                     loadingLabel: 'Salvando',
-                    onclick: () => {}
+                    variant: 'secondary',
+                    loading: false,
+                    type: 'submit',
+                    form: 'form-criar-pasta'
                 }
             ]}
         >
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Nome da Pasta</Form.Label>
-                    <Form.Control type="text" placeholder="Ex: Matematica" />
+                    <Form.Control type="text" placeholder="Ex: Matematica" value={folderName} onchange={handleChange} />
                 </Form.Group>
             </Form>
         </Modal>
